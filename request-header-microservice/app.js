99928b4b039a79db33f01129bf7a00f3888f4ad8
Gustavo Azevedo <gustavo_azevedo_7@hotmail.com>
2017-11-17 20:03:18 -0200

    Initial commit

---
+++ request-header-microservice/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.render('index.ejs')
+})
+
+app.get('/api/whoami', (req, res) => {
+  const data = {
+    ip: req.headers['x-forwarded-for'] || req.ip,
+    language: req.headers['accept-language'].split(',')[0],
+    software: req.headers['user-agent'].split('(')[1].split(')')[0]
+  }
+  res.render('whoami.ejs', {data: data})
+})
+
+app.listen(process.env.PORT || 5555, function() {
+  console.log('Server is running...')
+})
