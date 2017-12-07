1b7814dacb0830df6d419758cf714a00b06082fd
Gustavo Azevedo <gustavo_azevedo_7@hotmail.com>
2017-12-06 22:04:49 -0200

    Initial commit

---
+++ url-shortener-microservice/app.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const validUrl = require('valid-url')
+var counter = 0;
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/url-shortener', { useMongoClient: true })
+.then(() => console.log('Database connected successfully.'))
+.catch(err => console.log('Error connecting to database: ' + err.message))
+const urls = mongoose.connection.collection('urls');
+
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/', (req, res) => {
+  // Invalid URL
+  if (!validUrl.isUri(req.body.URL)) return res.render('shortened', {error: 'Invalid URL'})
+  // Valid URL
+    // URL already registered in our database
+  urls.findOne({originalUrl: req.body.URL}, (err, data) => {
+    if (err) throw err;
+    if (data) return res.render('shortened', {
+      originalUrl: data.originalUrl, 
+      shortUrl: `localhost:3333/${data.counter}`, 
+      error: null
+    })
+  })
+    // URL not registered in out database
+  counter++;
+  urls.insertOne({originalUrl: req.body.URL, counter: counter}, (err, data) => {
+    if (err) throw err;
+    return res.render('shortened', {
+      originalUrl: req.body.URL, 
+      shortUrl: `localhost:3333/${counter}`, 
+      error: null
+    })
+  })
+})
+
+app.get('/:num', (req, res) => {
+  const num = Number(req.params.num)
+  urls.findOne({counter: num}, (err, data) => {
+    if (err) throw err;
+    if (!data) return res.render('shortened', {error: 'URL not found'})
+    return res.redirect(data.originalUrl)
+  })
+})
+
+app.listen(3333, () => console.log('Server is up and running...'))
